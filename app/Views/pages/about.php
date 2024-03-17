d4dc1d84753dd162ec420fdca8958065868ded4d
imamady07 <imamadyy07@gmail.com>
2024-03-17 22:32:57 +0700

    trying

---
+++ app/Views/pages/about.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Tentang kami</h1>
+            <p>Universitas Pesantren Tinggi Darul 'Ulum adalah perguruan tinggi swasta yang didirikan oleh K.H Muhammad
+                As'ad Umar
+                dibawah naungan Yayasan Pesantren Tinggi Darul Ulum yang berada di Komplek
+                Pondok Pesantren Darul 'Ulum, Peterongan, Jombang, Jawa Timur.</p>
+
+            <?php 
+            d($tes);
+            ?>
+
+        </div>
+    </div>
+</div>
